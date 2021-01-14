53fce247db10897042086750a43d950ea8e4d3e2
AngelContador <acontador@institutmvm.cat>
2021-01-14 16:54:43 +0100

    lista

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    word = "python"
+    print(word[2])
+    print(word[-1])
+    print(len(word))
+    print(word[-6])
+    print(word[0:2])
+    print(word[2:])
+    print(word[2:6])
+    print(word[2:-1])
+    print(word[-2:])
+    print(word[:99])  #nofuncionarà
+    print(word[99:0]) #nofuncionarà
+    print(word[::-1])
+
+
+if __name__ == '__main__':
+    main()
